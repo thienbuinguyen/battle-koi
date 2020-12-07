949c6bcc0534edb42b11a1ed9522fa581bb806ca
Thien Bui-Nguyen <thien.minh.bui.nguyen@gmail.com>
2020-12-07 23:12:15 +1100

    add materialui, react router, skeleton home and about pages

---
+++ src/components/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function About() {
+  return (
+    <p>The about page is under construction...</p>
+  )
+}
+
+export default About;
---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <p>The home page is under construction...</p>
+  )
+}
+
+export default Home;
